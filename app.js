1721351c25a5142b4bf219245f45880241b6fb73
onticinc <onticinc@gmail.com>
2021-11-08 23:02:50 -0700

    put cards in grid

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 console.log('[app.js] is working.');
 
 
+// set variables
+
+let player1 = 'null';
+let player2 = 'null';
+
+let score = 'null';
+let cardValue = 10;
+
+
+
+// initialize board with all of the cards set upside down. 
+
+// flip card
+
+// check to see if cards match
+
+// if cards match leave right side up and add to score. 
+
+// alert card match
+
+// alert winner
+

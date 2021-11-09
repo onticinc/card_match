b6a891ea1bdf7bd358955e3360c1a7693f4f052b
onticinc <onticinc@gmail.com>
2021-11-09 12:33:09 -0700

    fixed header pushing before lunch.

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ let player2 = 'null';
 
 let score = 'null';
 let cardValue = 10;
+let card = document.querySelectorAll('card');
 
 
+// Start Game Button initialize board with all of the cards set upside down. 
 
-// initialize board with all of the cards set upside down. 
+    function startGame(){
+        document.querySelector('startGame').onclick
+        if (card.)
+
+    }
+
+    // use js to populate flex and grid area. Create object with FA icon values and card numbers. 
 
 // flip card
+function flipCard() {
+      document.getElementById("search-form").classList.toggle("hidden");
+      document.querySelector("#results-container").classList.toggle("hidden");
+    }
+
+
 
 // check to see if cards match
 

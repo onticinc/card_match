dbed322be439968ee07452ece1f0475bf5d2309f
onticinc <onticinc@gmail.com>
2021-11-10 15:49:54 -0700

    got inputs on form working

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log('[app.js] is working.');
 
-
 // set variables
-
 let player1 = 'null';
 let player2 = 'null';
 let mainScore = 'null';
@@ -10,6 +8,13 @@ let player1Score = 'null'
 let player2Score = 'null'
 let cardValue = 10;
 let card = document.querySelectorAll('card');
+
+function getSelectedTextValue() {
+    let  = document.getElementById("");
+    var selectedText = ddlFruits.options[ddlFruits.selectedIndex].innerHTML;
+    var selectedValue = ddlFruits.value;
+    alert("Selected Text: " + selectedText + " Value: " + selectedValue);
+}
 
 // Start Game Button.
 function startGame(){
@@ -21,18 +26,18 @@ function startGame(){
     let player2 = document.getElementById('player-two-input').value; 
 
     // Set Show Hide Card Number
-    let showHide = document.getElementById('player-two-input').value;
-    
-    // if (player2 = null) {
-    //     document.querySelector('');
-    // } else {
-    //     document.querySelector('');
-    // }
+   let showHide = document.getElementById('show-hide').value;
+
+    if (player2 = null) {
+        document.querySelector('');
+    } else {
+        document.querySelector('');
+    }
 
     // let gameText;
     //gameText = "Hello " + player1 + player2 + "Are you ready to play?";
     
-    console.log(player1, player2);
+    console.log(player1, player2, showHide);
 }
     // DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
 

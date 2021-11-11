2732e2b0634d04c087e53d149059bfaaf75e0d04
onticinc <onticinc@gmail.com>
2021-11-11 16:55:43 -0700

    working on game  header grid layout

--- app.js
+++ app.js
@@ -9,43 +9,35 @@ let player2Score = 'null';
 let cardValue = 10;
 const card = document.querySelectorAll('card');
 const playerOneName = document.getElementById('player-one-name');
-const playerTwoName = documentt.getElementById('player-two-name');
-const onePlayerScore = document.getElementById("player-one-score");
-const twoPlayerScore = document.getElementById("player-two-score");
+const playerTwoName = document.getElementById('player-two-name');
+const playerOneScore = document.getElementById("player-one-score");
+const playerTwoScore = document.getElementById("player-two-score");
 const buttons = document.getElementById('game-buttons');
 
 
 // Start Game Button.
 function startGame() {
-
     // Set player 1 name.
-    let player1 = document.getElementById('player-one-input').value;
+    let playerOneInput = document.getElementById('player-one-input').value;
     // Set player 2 name. 
     let player2 = document.getElementById('player-two-input').value;
     // Set show hide on front of the card. 
     let showHideSelector = document.getElementById('show-hide-selector').value;
 
-    console.log(player1, player2, showHideSelector);
-
     // One Player Mode = Show Game Buttons, total score, player one name. 
-    if (player2 = null) {
-        buttons.classList.toggle("hidden");
-        .innterHtml = 
-        onePlayerScore.classList.toggle("hidden");
-    }
+    // if (player2 = null) {
+    //     playerOneName.innerText = playerOneInput;
+    //     buttons.classList.toggle("hidden"); 
+    //     playerOneScore.classList.toggle("hidden");
+    // }
 
 }
-
-
-
-
 
 function showHide() {
     buttons.classList.toggle("hidden");
     onePlayerScore.classList.toggle("hidden");
     console.log(buttons);
 }
-
 
     // If player two is empty and show card number is selected, Toggle: show-front of card, one player score, game buttons, and cards. 
 

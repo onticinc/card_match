59044cffa29ddcbb293707731a5976c2ca022eb8
onticinc <onticinc@gmail.com>
2021-11-11 13:49:46 -0700

    FINALLYgit add .  got the show hide function to work

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ console.log('[app.js] is working.');
 let player1 = 'null';
 let player2 = 'null';
 let mainScore = 'null';
-let player1Score = 'null'
-let player2Score = 'null'
+let player1Score = 'null';
+let player2Score = 'null';
 let cardValue = 10;
-let card = document.querySelectorAll('card');
-let buttons = document.querySelector('game-buttons');
+const card = document.querySelectorAll('card');
+const buttons = document.getElementById('game-buttons');
+const onePlayerScore = document.getElementById("one-player-score");
+const twoPlayerScore = document.getElementById("two-player-score");
 
 // Start Game Button.
 function startGame() {
@@ -19,14 +21,28 @@ function startGame() {
     let player2 = document.getElementById('player-two-input').value;
     // Set show hide on front of the card. 
     let showHideSelector = document.getElementById('show-hide-selector').value;
-    
-    // // If player two is empty and show card number is selected, Toggle: show-front of card, one player score, game buttons, and cards. 
-    // if (player2 = null && (showHide = 1)) {
-    //     document.querySelector("one-player-score").classList.toggle("hidden");
-    //     document.querySelector("game-buttons").classList.toggle("hidden");
+
+    console.log(player1, player2, showHideSelector);
+
+} // works up to here. 
 
 
-    // // If player two is empty and hide card number is selected, Toggle: one player score, game buttons, and cards.
+function showHide(){
+    buttons.classList.toggle("hidden");
+    onePlayerScore.classList.toggle("hidden");
+    console.log(buttons);
+}
+
+
+    // If player two is empty and show card number is selected, Toggle: show-front of card, one player score, game buttons, and cards. 
+    //if (player2 = null) {
+
+
+
+    //}
+
+    // if {
+    // // 
     // } else if (player2 = null && (showHide = 2)) {
     //     document.querySelector("").classList.toggle("hidden");
     //     document.querySelector("game-buttons").classList.toggle("hidden");
@@ -35,17 +51,17 @@ function startGame() {
     // // If player two is not null, show game buttons, two player score, 
     // }
 
-    
 
-    console.log(player1, player2, showHideSelector );
-}
+
+
+
 // DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
 
 // flip card
-function flipCard() {
-    document.querySelectorAll("card-back").classList.toggle("hidden");
-    document.querySelectorAll("card-front").classList.toggle("hidden");
-}
+// function flipCard() {
+//     document.querySelectorAll("card-back").classList.toggle("hidden");
+//     document.querySelectorAll("card-front").classList.toggle("hidden");
+// }
 
 // check to see if cards match
 

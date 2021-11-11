5413abf5075155bf1963eebf17d880a2d19b09ad
onticinc <onticinc@gmail.com>
2021-11-10 17:11:52 -0700

    random update. Working on show hide js.

--- app.js
+++ app.js
@@ -8,38 +8,33 @@ let player1Score = 'null'
 let player2Score = 'null'
 let cardValue = 10;
 let card = document.querySelectorAll('card');
-
-function getSelectedTextValue() {
-    let  = document.getElementById("");
-    var selectedText = ddlFruits.options[ddlFruits.selectedIndex].innerHTML;
-    var selectedValue = ddlFruits.value;
-    alert("Selected Text: " + selectedText + " Value: " + selectedValue);
-}
+let buttons = document.querySelector('game-buttons');
 
 // Start Game Button.
-function startGame(){
+function startGame() {
 
     // Set player 1 name.
     let player1 = document.getElementById('player-one-input').value;
-
     // Set player 2 name. 
-    let player2 = document.getElementById('player-two-input').value; 
+    let player2 = document.getElementById('player-two-input').value;
+    // Set show hide on front of the card. 
+    let showHide = document.getElementById('show-hide').value;
 
-    // Set Show Hide Card Number
-   let showHide = document.getElementById('show-hide').value;
 
-    if (player2 = null) {
-        document.querySelector('');
-    } else {
-        document.querySelector('');
-    }
+    // if (player2 = null && (showHide = 1 )) {
+    //     document.querySelector("one-player-score").classList.toggle("hidden");
+    //     document.querySelector("game-buttons").classList.toggle("hidden");
+    // } else {
+    //     document.querySelector("two-player-score").classList.toggle("hidden");
+    //     document.querySelector("game-buttons").classList.toggle("hidden");
+    // }
 
     // let gameText;
     //gameText = "Hello " + player1 + player2 + "Are you ready to play?";
-    
+
     console.log(player1, player2, showHide);
 }
-    // DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
+// DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
 
 // flip card
 function flipCard() {
@@ -51,7 +46,7 @@ function flipCard() {
 
 
 
-// if cards match leave right side up and add to score. 
+// if cards match leave right side up  and add to score. 
 
 
 

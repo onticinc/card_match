c37e15407e9ea734c3fd61c649ec75bf413ff065
onticinc <onticinc@gmail.com>
2021-11-10 20:52:15 -0700

    working on start game function

--- app.js
+++ app.js
@@ -18,21 +18,26 @@ function startGame() {
     // Set player 2 name. 
     let player2 = document.getElementById('player-two-input').value;
     // Set show hide on front of the card. 
-    let showHide = document.getElementById('show-hide').value;
-
-
-    // if (player2 = null && (showHide = 1 )) {
+    let showHideSelector = document.getElementById('show-hide-selector').value;
+    
+    // // If player two is empty and show card number is selected, Toggle: show-front of card, one player score, game buttons, and cards. 
+    // if (player2 = null && (showHide = 1)) {
     //     document.querySelector("one-player-score").classList.toggle("hidden");
     //     document.querySelector("game-buttons").classList.toggle("hidden");
-    // } else {
-    //     document.querySelector("two-player-score").classList.toggle("hidden");
+
+
+    // // If player two is empty and hide card number is selected, Toggle: one player score, game buttons, and cards.
+    // } else if (player2 = null && (showHide = 2)) {
+    //     document.querySelector("").classList.toggle("hidden");
     //     document.querySelector("game-buttons").classList.toggle("hidden");
+
+    // } else {
+    // // If player two is not null, show game buttons, two player score, 
     // }
 
-    // let gameText;
-    //gameText = "Hello " + player1 + player2 + "Are you ready to play?";
+    
 
-    console.log(player1, player2, showHide);
+    console.log(player1, player2, showHideSelector );
 }
 // DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
 

d77ba0948fff2c73ff7225551020e01e7d004160
onticinc <onticinc@gmail.com>
2021-11-12 21:59:40 -0700

    got Player 1 and Player 2 to show if null on input

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('[app.js] is working.');
 // set variables
 let mainScore = 'null';
 let cardValue = 10;
+let clickedCard = '';
 const inputForm = document.getElementById('input-form');
 const gameHeader = document.getElementById('game-header');
 const cardContainer = document.getElementById('card-table');
@@ -29,29 +30,22 @@ function startGame() {
     playerOneName.innerText = (playerOneInput + ": " + playerOneScore);
     playerTwoName.innerText = (playerTwoInput + ": " + playerTwoScore);
 
-    // // If null print player 1 and player 2. 
-    // if (playerOneName === null) {
-    //     playerOneName.innerText = ("Player 1:" + playerOneScore);
-    //     console.log(playerOneName);
-    // }
 
-    // if (playerTwoName === null) {
-    //     playerTwoName.innterText = ("Player 2:" + playerTwoScore);
-    // }
 
-// check for value in name input. 
-//     if (inputtx.value.length == 0)
-//     { 
-//        alert("message");  	
-//        return false; 
-//     }  	
-//     return true; 
-//   } 
+    // If null print player 1 and player 2. 
+    if (playerOneInput === '') {
+        playerOneName.innerText = ("Player 1: " + playerOneScore);
+    }
+
+    if (playerTwoInput === '') {
+        playerTwoName.innerText = ("Player 2: " + playerTwoScore);
+    }
+
+
 
     // show header.
     gameHeader.classList.toggle("hidden");
     cardContainer.classList.toggle("hidden");
-    console.log(playerOneName, playerTwoName, showHideSelector);
 
     // hide card numbers. 
     if (showHideSelector === '2') {
@@ -71,7 +65,7 @@ function showHideCard() {
     for (let idx = 0; idx < cardList.length; idx++) {
         let card = cardList[idx];
         card.addEventListener('click', function () {
-            console.log('click', "click on card");
+            // console.log('click', "click on card");
             let firstChild = card.children[0];
             let secondChild = card.children[1];
             firstChild.classList.toggle('hidden');
@@ -80,18 +74,123 @@ function showHideCard() {
     }
 }
 
-showHideCard();
 
-function compareCards(){
 
-}
+// //Tic Tac Toe
+// //Global Variables
+// //Get input from user about which sign should be for playerOne.
+// //Set signs for both players.
+// //When user click on button it triggers play function()
+// //Play function detects who is playing this turn.
+// //Display Sign on that button accordingly.
+// var playerOneChoice;
+// var playerTwoChoice;
+// var playerTurn=1;
+// var matrix = [[], [], []];
+// var buttons = [[], [], []];
+// var row1;
+// var X = "X";
+// var O = "O";
+// var x;
+// var y;
+// var i;
+
+// //Input the matrix
+// for (i = 0; i <= 8; i++) {
+// 	switch (i) {
+// 	case 0: x=0; y=0; break;
+// 	case 1: x=0; y=1; break;
+// 	case 2: x=0; y=2; break;
+// 	case 3: x=1; y=0; break;
+// 	case 4: x=1; y=1; break;
+// 	case 5: x=1; y=2; break;
+// 	case 6: x=2; y=0; break;
+// 	case 7: x=2; y=1; break;
+// 	case 8: x=2; y=2; break;
+// 	}
+
+// 	matrix[x][y] = document.querySelector(".cell"+i);
+// 	buttons[x][y] = document.querySelector("#b"+i);
+// 	//matrix[x][y].innerHTML = i + " " + x + " " + y;
+// }
+
+// function play() {
+// displayOutput();
+// incPlayerTurn();
+// }
+
+// function displayTurn() {
+//     var display = document.querySelector("#playerTurn");
+//     if (playerTurn%2==0)
+//         display.innerHTML ="Its player two turn";
+// 	else
+// 		display.innerHTML ="Its player one turn";
+// }    
+// function incPlayerTurn(){
+//     playerTurn++;
+// }
+
+// function displayOutput(){
+// 	if (playerTurn%2 == 1)
+// 		this.innerHTML = playerOneChoice;
+// 	else
+// 		this.innerHTML = playerTwoChoice;
+// }
+
+
+// function setO(){
+// 	playerOneChoice="O";
+// 	playerTwoChoice="X"
+// }
+// function setX(){
+// 	playerOneChoice="X";
+// 	playerTwoChoice="O"
+// }
+
+// function displayO(){
+// var butt = document.getElementsByTagName("button");
+// var buttonsCount = buttons.length;
+// for (var i = 0; i <= buttonsCount; i += 1) {
+//  butt[i].onclick = function(e) {
+//  this.innerHTML ="O";
+//     };
+// }
+// }
+
+// function displayX(){
+// var butt = document.getElementsByTagName("button");
+// var buttonsCount = buttons.length;
+// for (var i = 0; i <= buttonsCount; i += 1) {
+//     butt[i].onclick = function(e) {
+//  this.innerHTML ="X";
+//     };
+// }
+// }
+
+// playerchoice.addEventListener("change", function(event) {
+//     if (event.target.value === "O")
+//         players.reverse();
+
+//     this.disabled = true;
+//     playarea.style.display = "block";
+
+//     displayTurn();
+// }, false);
+
+
+
+// function compareCards(){
+    
+    
+//     let currentPlayer =
+//     let cardOneId = 'j';
+//     let cartTwoId = 'j';
+//     showHideCard();
+// }
 
 
 
 // card i just clicked
-function getId(){
-    
-}
 
 // last card I clicked
 
@@ -99,40 +198,11 @@ function getId(){
 
 // add points to player. 
 
-// // hide cards. 
-// if (showHideSelector === '2') {
-//     
-// };
-
-// Figure out how to flip and turn one 
 
 
-    // If player two is empty and show card number is selected, Toggle: show-front of card, one player score, game buttons, and cards. 
-
-
-    //}
-
-    // if {
-    // // 
-    // } else if (player2 = null && (showHide = 2)) {
-    //     document.querySelector("").classList.toggle("hidden");
-    //     document.querySelector("game-buttons").classList.toggle("hidden");
-
-    // } else {
-    // // If player two is not null, show game buttons, two player score, 
-    // }
 
 // DO LATER. use js to populate flex and grid area. Create object with FA icon values and card numbers. Randomize the board.  
 
-// flip card
-// function flipCard() {
-//     document.querySelectorAll("card-back").classList.toggle("hidden");
-//     document.querySelectorAll("card-front").classList.toggle("hidden");
-// }
-
-// check to see if cards match
-
-// if cards match leave right side up  and add to score. 
 
 
 
